700f4f35412436907624f14d25e42a590a5425f6
mohamed saied <127026703+mohamedsaied22@users.noreply.github.com>
2023-05-19 19:56:43 +0300

    some edites for components

---
+++ src/components/footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './Footer.css';
+
+const Footer = () => {
+  return (
+    <div>
+      <footer>
+        Designed and developed by Mohappy.com
+        <span>🧡</span>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/components/mainContent.js
@@ -0,0 +1,14 @@
+
+
+import React from 'react';
+import './MainContent.css';
+
+const MainContent = ({pageName}) => {
+  return (
+    <main>
+      {pageName} 
+    </main>
+  );
+}
+
+export default MainContent;
